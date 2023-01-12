9fef74f2419fd11a4e74cd0f39207a5ce63534bb
23NateK <n.kimber22@gmail.com>
2023-01-12 11:38:10 -0600

    Started coding Quadratic

--- Quadratic.java
+++ Quadratic.java
@@ -1,5 +1,17 @@
 public class Quadratic {
     public static void main(String[] args) {
-        
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
+        if (a == 1 ){
+            System.out.println(-c/b);
+        }else if (b == 1){
+            double result1 = Math.sqrt(-c/b);
+            double result2 = -1*(Math.sqrt(-c/b));
+            System.out.println(result1 + ", " + result2);
+        }else{
+            
+        }
+
     }
 }
