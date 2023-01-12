7ba349618eea9b4b3b449a98fde5f6b143289961
23NateK <n.kimber22@gmail.com>
2023-01-12 12:06:14 -0600

    Finished ThreeEqual

--- ThreeEqual.java
+++ ThreeEqual.java
@@ -1,5 +1,16 @@
 public class ThreeEqual {
     public static void main(String[] args) {
-        
+        if (args.length<3){
+            System.out.println("not enough variables");
+        }else{
+            double a = Double.parseDouble(args[0]);
+            double b = Double.parseDouble(args[1]);
+            double c = Double.parseDouble(args[2]);
+            if (a-b == b-c){
+                System.out.println("equal");
+            }else{
+                System.out.println("not equal");
+            }
+        }
     }
 }
