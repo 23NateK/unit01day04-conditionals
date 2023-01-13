b100fe77ef1787a9500c9cb4cb74849129e4a8d8
23NateK <n.kimber22@gmail.com>
2023-01-13 12:21:43 -0600

    Fixed Quadratic and ThreeEqual

--- Quadratic.java
+++ Quadratic.java
@@ -3,18 +3,21 @@ public class Quadratic {
         double a = Double.parseDouble(args[0]);
         double b = Double.parseDouble(args[1]);
         double c = Double.parseDouble(args[2]);
-        if (a == 0 ){
-            System.out.println(-c/b);
+        if ((b*b)-4*a*c <= 0){
+            System.out.println("No Solution");
         }else{
-            double answer1 = Math.sqrt((b*b)-4*a*c);
-            answer1 = -b + answer1;
-            answer1 = answer1/(2*a);
-            System.out.println(answer1);
-            double answer2 = Math.sqrt((b*b)-4*a*c);
-            answer2 = -b - answer2;
-            answer2 = answer2/(2*a);
-            System.out.println(answer2);
+            if (a == 0 ){
+                System.out.println(-c/b);
+            }else{
+                double answer1 = Math.sqrt((b*b)-4*a*c);
+                answer1 = -b + answer1;
+                answer1 = answer1/(2*a);
+                System.out.println(answer1);
+                double answer2 = Math.sqrt((b*b)-4*a*c);
+                answer2 = -b - answer2;
+                answer2 = answer2/(2*a);
+                System.out.println(answer2);
+            }
         }
-
     }
 }
--- ThreeEqual.java
+++ ThreeEqual.java
@@ -6,7 +6,7 @@ public class ThreeEqual {
             double a = Double.parseDouble(args[0]);
             double b = Double.parseDouble(args[1]);
             double c = Double.parseDouble(args[2]);
-            if (a-b == b-c){
+            if (a == b && b==c){
                 System.out.println("equal");
             }else{
                 System.out.println("not equal");
