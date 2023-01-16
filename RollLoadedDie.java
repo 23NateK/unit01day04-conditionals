1c4a9027760655cbb2293acd5c70d3a7f81a8796
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-15 22:06:46 -0600

    RollLoadedDie.java feedback

--- RollLoadedDie.java
+++ RollLoadedDie.java
@@ -1,3 +1,4 @@
+// good. I think this is the most elegant solution I've seen
 public class RollLoadedDie {
     public static void main(String[] args) {
         double roll = Math.round(Math.random()*7+1);
